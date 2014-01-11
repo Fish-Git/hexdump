6036007a0d84e12fe53f527a7f162b68e343a7c7
Fish (David B. Trout) <fish@infidels.org>
2014-01-11 14:55:44 -0800

    Initial commit

---
+++ stdafx.h
@@ -0,0 +1,23 @@
+
+// Common #includes...
+
+#pragma once
+
+#define  _CRT_SECURE_NO_WARNINGS  1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "typedefs.h"   // (BYTE, U32, U64, etc)
+
+#ifndef TRUE
+#define TRUE  1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#ifndef INLINE
+#define INLINE  __inline
+#endif
---
+++ typedefs.h
@@ -0,0 +1,17 @@
+
+#pragma once
+
+typedef   signed __int8  S8;
+typedef unsigned __int8  U8;
+
+typedef   signed __int16 S16;
+typedef unsigned __int16 U16;
+
+typedef   signed __int32 S32;
+typedef unsigned __int32 U32;
+
+typedef   signed __int64 S64;
+typedef unsigned __int64 U64;
+
+typedef U8 BYTE;
+typedef U8 bool;
